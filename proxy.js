758c7ca4311fdc8b0e269ebfda80a586ebb07a9f
Serg <serj.nights@live.com>
2016-09-28 17:00:53 -0400

    for better logging

--- proxy.js
+++ proxy.js
@@ -9,8 +9,12 @@ var options = {
     'lanks.nightspeller.net': 'http://127.0.0.1:4000'
 };
 
+var prevHost = '';
 http.createServer(function(req, res) {
-    console.log(req.headers.host);
+    if (req.headers.host !== prevHost) {
+        console.log(req.headers.host+'\r\n');
+        prevHost = req.headers.host;
+    }
     var target = options[req.headers.host] ? options[req.headers.host] : 'http://127.0.0.1:3001';
     proxy.web(req, res, {
         target: target
