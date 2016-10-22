242b6672aacc53f3fedcc256e2bb663a7759aee7
Sergei Sidorov <serj.nights@live.com>
2016-10-22 00:46:58 +0000

    logist36 domains added

--- proxy.js
+++ proxy.js
@@ -8,7 +8,10 @@ var options = {
     'nightspeller.net': 'http://127.0.0.1:3001',
     'streams.nightspeller.net': 'http://127.0.0.1:3002',
     'xn----7sbhdzihcxec9an7e.xn--p1ai': 'http://127.0.0.1:3003', //logistics-school
-    'starjourney.nightspeller.net': 'http://127.0.0.1:3004'
+    'starjourney.nightspeller.net': 'http://127.0.0.1:3004',
+    'logistvrn.ru': 'http://127.0.0.1:3005',
+    'logist36.ru': 'http://127.0.0.1:3005',
+    'xn--36-glcunsxi.xn--p1ai': 'http://127.0.0.1:3005'
 };
 
 var prevHost = '';
